0d489f9265503b90d327dbdfc5279cdba98d5d27
alanzin1 <alanvictorapt@gmail.com>
2026-07-31 10:47:36 -0300

    a

--- src/sections/hero/Hero.jsx
+++ src/sections/hero/Hero.jsx
@@ -15,6 +15,7 @@ export default function Hero() {
           <span>transforma</span>
           <br />
           vidas
+          <LuHeart className={styles.heartTitle} />
         </h1>
 
         <p className={styles.subtitle}>
